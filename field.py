9896322c0979d5eec75461c5350ef89b589f0956
Oleksandr <115171014+OleksandrPiatnytsia@users.noreply.github.com>
2023-11-07 20:58:59 +0200

    abstract method added

--- field.py
+++ field.py
@@ -1,25 +1,26 @@
 from abc import ABC, abstractmethod
 
-class Field(ABC):
 
-    @abstractmethod
-    def __getitem__(self):
-        pass
+class Field(ABC):
 
     def __init__(self, value):
         self.value = value
-    
+
+    @abstractmethod
+    def is_valid(self, value):
+        pass
+
     @property
     def value(self):
         return self.__value
-    
+
     @value.setter
     def value(self, value):
         if self.is_valid(value):
             self.__value = value
-    
+
     def __str__(self):
         return str(self.value)
-    
+
     def __repr__(self):
         return repr(self.value)
